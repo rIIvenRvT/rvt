9970d9d80ab99114bbf299aa7aafe77744d15081
rIIvenRvT <97190320+rIIvenRvT@users.noreply.github.com>
2022-04-09 22:13:00 -0300

    Update name.lua

--- name.lua
+++ name.lua
@@ -13,8 +13,8 @@ u8 = encoding.UTF8
 
 update_state = false
 
-local script_vers = 2
-local script_vers_text = "1.05"
+local script_vers = 1
+local script_vers_text = "1.00"
 
 local update_url = "https://raw.githubusercontent.com/rIIvenRvT/rvt/main/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
