93172de9946169a6a7d7284bba681af593e7494c
rIIvenRvT <97190320+rIIvenRvT@users.noreply.github.com>
2022-05-18 18:48:21 -0300

    Update Mimgui.lua

--- Mimgui.lua
+++ Mimgui.lua
@@ -1,5 +1,5 @@
 script_name("rIIven")
-script_version_number(7)
+script_version_number(8)
 
 local imgui, ffi = require 'mimgui', require 'ffi'
 local new, str = imgui.new, ffi.string
@@ -31,7 +31,7 @@ local menu = {
     u8'Troller',
     u8'Teleportes',
     u8'Veiculos',
-    u8'Teleportes',
+    u8'AAAAAAAAA',
 }
 
 function main()
